4f2a1b6ecd2de764e9cdde4fe13f4742a7b0dcad
John Bretherton <270022735@bun2-206-02.local>
2019-05-14 15:24:55 +1200

    Working on 7 and 8

--- homeworkTasks/task7/js/script.js
+++ homeworkTasks/task7/js/script.js
@@ -1,4 +1,28 @@
 // TASK 7: Create a script that calculates the average number from an array
-	// ◦	Numbers then can be added from prompt and new average given
+	// ◦	Numbers then can be added from a prompt and a new average can be given
 
-  
+var numArray = [25, 55, 35, 45, 95, 75, 65, 85];
+var numTotal = 0;
+// console.log(numArray);
+
+// FIRST PART:
+// for (var i = 0; i < numArray.length; i++) {
+//   numTotal = numTotal + numArray[i];
+// }
+// var numAverage = numTotal / numArray.length;
+// document.getElementById("startPara").innerHTML = "The average is: " + numAverage;
+
+
+// SECOND PART:
+for (var i = 0; i < numArray.length; i++) {
+  numTotal = numTotal + numArray[i];
+}
+var numAverage = numTotal / numArray.length;
+
+function addNumber(){
+  var newNumber = parseInt(prompt("Add a new number and I'll calculate the new average!"));
+  numArray.push(newNumber);
+  numTotal = numTotal += newNumber;
+  var numAverage = numTotal / numArray.length;
+  document.getElementById("newPara").innerHTML = "After adding your number, the new average is " + numAverage;
+}
--- homeworkTasks/task8/js/script.js
+++ homeworkTasks/task8/js/script.js
@@ -1,3 +1,19 @@
 // TASK 8: Create a script that asks for x amount of people.
 // And then asks for their ages and finds the oldest and youngest age.
 // As well as the average age
+
+var peopleGroup = [];
+peopleGroup.length = 5;
+
+function addNames() = {
+  var peopleX = parseInt(prompt("Please give me a name."));
+
+  for (var i = 0; i < peopleGroup.length; i++) {
+    peopleGroup.push(peopleX);
+  }
+}
+console.log(peopleGroup);
+
+addNames();
+
+// NOT WORKING YET!
