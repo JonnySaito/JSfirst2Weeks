5ed382cbcd8d8a1e665eb559d7d0038b5ecad4de
John Bretherton <270022735@bun2-206-02.local>
2019-05-15 15:32:01 +1200

    Still working on 8

--- homeworkTasks/task8/js/script.js
+++ homeworkTasks/task8/js/script.js
@@ -1,19 +1,35 @@
-// TASK 8: Create a script that asks for x amount of people.
-// And then asks for their ages and finds the oldest and youngest age.
-// As well as the average age
+// TASK 8:
+// (1) Create a script that asks for x amount of people.
+// (2) Asks for their ages.
+// (3) Then finds the oldest and youngest ages.
+// (4) Then finds the average age.
 
-var peopleGroup = [];
-peopleGroup.length = 5;
+var classSize = parseInt(prompt("How many people are in your class?"));
+var classNames = [];
+var classAges = [];
+var oldestAge = 0;
+var youngestAge = 100;
 
-function addNames() = {
-  var peopleX = parseInt(prompt("Please give me a name."));
+function addClassmate(){
+  for (var i = 0; i < classSize; i++){
+    var nameX = prompt("Please provide a classmate's name.");
+    classNames.push(nameX);
 
-  for (var i = 0; i < peopleGroup.length; i++) {
-    peopleGroup.push(peopleX);
+    var ageX = parseInt(prompt("What is their age?"));
+    classAges.push(ageX);
+    document.getElementById("classMembers").innerHTML += "<li>" + classNames[i] + " is " + classAges[i] + " years old.</li>";
   }
 }
-console.log(peopleGroup);
 
-addNames();
-
-// NOT WORKING YET!
+function classAges(){
+  for (var i = 0; i < classSize; i++){
+    if (classAges[i] < youngestAge){
+      youngestAge = classAges[i];
+      document.getElementById("oldestYoungest").innerHTML += "<li>The youngest age in the class is " + youngestAge + "<li>";
+    }
+    if (classAges[i] > oldestAge){
+      oldestAge = classAges[i];
+      document.getElementById("oldestYoungest").innerHTML += "<li>The oldest age in the class is " + oldestAge + "<li>";
+    }
+  }
+}
