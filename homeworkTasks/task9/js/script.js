78742bdf1a1739fd7871eb9f741183a251cda963
John Bretherton <270022735@bun2-204-16.local>
2019-05-17 16:45:44 +1200

    Still working on exercise 9

--- homeworkTasks/task9/js/script.js
+++ homeworkTasks/task9/js/script.js
@@ -3,25 +3,28 @@
 // It should be able to do all 4 mathematical options
 var firstNumber = parseInt(prompt("Please give me a number"));
 var secondNumber = parseInt(prompt("Please give me another number"));
-var newNumber1 = 0;
+var newNumber1, newNumber2, newNumber3, newNumber4;
 
-
+addNumbers();
 function addNumbers(){
     newNumber1 = firstNumber + secondNumber;
 }
 console.log(newNumber1);
 
-// function subtractNumbers(){
-//     newNumber2 = (firstNumber - secondNumber);
-//     console.log(newNumber2);
-// }
-//
-// function multiplyNumbers(){
-//     newNumber3 = (firstNumber * secondNumber);
-//     console.log(newNumber3);
-// }
-//
-// function divideNumbers(){
-//     newNumber4 = (firstNumber / secondNumber);
-//     console.log(newNumber4);
-// }
+function subtractNumbers(){
+    newNumber2 = (firstNumber - secondNumber);
+}
+console.log(newNumber2);
+
+function multiplyNumbers(){
+    newNumber3 = (firstNumber * secondNumber);
+}
+console.log(newNumber3);
+
+function divideNumbers(){
+    newNumber4 = (firstNumber / secondNumber);
+}
+console.log(newNumber4);
+
+
+// STILL NOT WORKING YET!
